1dc34f5e632d4ee8cf1e4597204b1fa6127c8226
oleewere <oleewere@gmail.com>
2014-10-20 16:34:01 +0200

    add rdd function tests

--- spark-samples/src/test/scala/com/sequenceiq/spark/RDDOperationsTest.scala
+++ spark-samples/src/test/scala/com/sequenceiq/spark/RDDOperationsTest.scala
@@ -23,7 +23,7 @@ class RDDOperationsTest {
 
   @Test
   def testMap() {
-    val input = Seq(1 ,2 ,3, 4)
+    val input = Seq(1, 2, 3, 4)
     val expectedOutput = Array("s1", "s2", "s3", "s4")
 
     val output = sc.makeRDD(input).map(num => "s" + num.toString())
@@ -34,7 +34,7 @@ class RDDOperationsTest {
 
   @Test
   def testFlatMap() {
-    val input = Seq(1 ,2 ,3, 4)
+    val input = Seq(1, 2, 3, 4)
     val expectedOutput = Array("s1", "t1", "s2", "t2", "s3", "t3", "s4", "t4")
 
     val output = sc.makeRDD(input).flatMap(num => List("s" + num.toString(), "t" + num.toString()))
@@ -44,8 +44,21 @@ class RDDOperationsTest {
   }
 
   @Test
+  def testAggregate() {
+    val input = Seq(1, 2, 3, 4)
+    val expectedOutput = 10
+
+    val output = sc.makeRDD(input).aggregate[Int](0)(
+      (x, y) => x + y, // aggregate function
+      (z, w) => z + w)
+
+    Assert.assertEquals(output, expectedOutput)
+
+  }
+
+  @Test
   def testFilter() {
-    val input = Seq(1 ,2 ,3, 4)
+    val input = Seq(1, 2, 3, 4)
     val expectedOutput = Array(1,2)
 
     val output = sc.makeRDD(input).filter(num => num < 3)
@@ -56,7 +69,7 @@ class RDDOperationsTest {
 
   @Test
   def testCount() {
-    val input = Seq(1 ,2 ,3, 4)
+    val input = Seq(1, 2, 3, 4)
     val expectedOutput = 4
 
     val output = sc.makeRDD(input).count()
@@ -77,6 +90,29 @@ class RDDOperationsTest {
   }
 
   @Test
+  def testMinMax() {
+    val input = Seq(1, 2, 3, 4)
+    val expectedMin = 1
+    val expectedMax = 4
+
+    val min = sc.makeRDD(input).min()
+    val max = sc.makeRDD(input).max()
+
+    Assert.assertEquals(min, expectedMin)
+    Assert.assertEquals(max, expectedMax)
+  }
+
+  @Test
+  def testSum() {
+    val input = Seq(1, 2, 3)
+    val expectedOutput = 6.0
+
+    val output = sc.makeRDD(input).sum()
+
+    Assert.assertEquals(output, expectedOutput)
+  }
+
+  @Test
   def testDistinct() {
     val input = Seq(1, 1, 2)
     val expectedOutput = Array(1, 2)
@@ -89,7 +125,7 @@ class RDDOperationsTest {
 
   @Test
   def testReduce() {
-    val input = Seq(1 ,2 ,3, 4)
+    val input = Seq(1, 2, 3, 4)
     val expectedOutput = 10
 
     val output = sc.makeRDD(input).reduce((n,m) => n + m)
@@ -99,17 +135,32 @@ class RDDOperationsTest {
 
   @Test
   def testGroupBy() {
-    val input = Seq((1, 1), (1, 2), (1,3), (2, 4))
-    val expectedOutput = Array((1, 1), (1, 2), (1,3)) // Groups: ((1, 1), (1, 2), (1,3)) and ((2, 4))
+    val input = Seq((1, 1), (1, 2), (1, 3), (2, 4))
+    val expectedOutput = Array(
+      (1, Iterable((1, 1), (1, 2), (1,3))),
+      (2, Iterable((2, 4)))
+    )
 
     val output = sc.makeRDD(input).groupBy(x => x._1) // group by first elements of the tuples
 
-    Assert.assertEquals(output.collect.apply(0)._2.toArray, expectedOutput)
+    Assert.assertEquals(output.collect(), expectedOutput)
+  }
+
+  @Test
+  def testForeachWithAccumulator() {
+    val input = Seq(1, 2, 3)
+    val expectedOutput = 6
+
+    val accumulatorValue = sc.accumulator(0, "My Accumulator")
+
+    sc.makeRDD(input).foreach(x => accumulatorValue += x)
+
+    Assert.assertEquals(accumulatorValue.value, expectedOutput)
   }
 
   @Test
   def testFirst() {
-    val input = Seq(1 ,2 ,3, 4)
+    val input = Seq(1, 2, 3, 4)
     val expectedOutput = 1
 
     val output = sc.makeRDD(input).first()
@@ -135,6 +186,17 @@ class RDDOperationsTest {
     val expectedOutput = Array(1,2)
 
     val output = sc.makeRDD(input).takeOrdered(2)
+
+    Assert.assertEquals(output, expectedOutput)
+
+  }
+
+  @Test
+  def testTopN() {
+    val input = Seq(4, 1, 2, 3)
+    val expectedOutput = Array(4,3)
+
+    val output = sc.makeRDD(input).top(2)
 
     Assert.assertEquals(output, expectedOutput)
 
@@ -262,6 +324,17 @@ class RDDOperationsTest {
 
   }
 
+  @Test
+  def testKeyBy() {
+    val input = Seq(1, 2, 3, 4)
+    val expectedOutput = Array(("1", 1), ("2", 2), ("3", 3), ("4", 4))
+
+    val output = sc.makeRDD(input).keyBy(x => x.toString)
+
+    Assert.assertEquals(output.collect(), expectedOutput)
+
+  }
+
   /// Pair RDD functions
 
   @Test
@@ -272,6 +345,20 @@ class RDDOperationsTest {
     val output = sc.makeRDD(input).lookup(1)
 
     Assert.assertEquals(output, expectedOutput)
+  }
+
+  @Test
+  def testCombineByKey() {
+    val input = Seq((1, 2), (2, 3), (2, 4), (1, 4))
+    val expectedOutput = Array((1, "24"), (2, "34"))
+
+    val output = sc.makeRDD(input).combineByKey[String](
+      (x: Int) => x.toString(), // combiner
+      (s: String, value: Int) => s + value.toString(), //merge value
+      (s1: String, s2: String) => s1 + s2 // merge combiners
+    )
+
+    Assert.assertEquals(output.collect(), expectedOutput)
   }
 
   @Test
@@ -295,12 +382,72 @@ class RDDOperationsTest {
   }
 
   @Test
+  def testFoldByKey() {
+    val input = Seq((1, 2), (1, 3), (2, 4))
+    val expectedOutput = Array((1,7), (2, 6))
+
+    val output = sc.makeRDD(input).foldByKey(2)(_ + _)
+
+    Assert.assertEquals(output.collect(), expectedOutput)
+  }
+
+  @Test
+  def testGroupByKey() {
+    val input = Seq((1, 1), (1, 2), (1, 3), (2, 4))
+    val expectedOutput = Array(
+      (1, Iterable(1, 2, 3)),
+      (2, Iterable(4))
+    )
+
+    val output = sc.makeRDD(input).groupByKey()
+
+    Assert.assertEquals(output.collect(), expectedOutput)
+  }
+
+  @Test
   def testJoin() {
     val input1 = sc.makeRDD(Seq((1, 4), (2, 5)))
     val input2 = sc.makeRDD(Seq((1, '1'), (2, '2')))
     val expectedOutput = Array((1, (4, '1')), (2, (5, '2')))
 
     val output = input1.join(input2)
+
+    Assert.assertEquals(output.collect(), expectedOutput)
+  }
+
+  @Test
+  def testRightOuterJoin() {
+    val input1 = sc.makeRDD(Seq((1, 4)))
+    val input2 = sc.makeRDD(Seq((1, '1'), (2, '2')))
+    val expectedOutput = Array((1, (Some(4), '1')), (2, (None, '2')))
+
+    val output = input1.rightOuterJoin(input2)
+
+    Assert.assertEquals(output.collect(), expectedOutput)
+  }
+
+  @Test
+  def testLeftOuterJoin() {
+    val input1 = sc.makeRDD(Seq((1, 4), (2, 5)))
+    val input2 = sc.makeRDD(Seq((1, '1')))
+    val expectedOutput = Array((1, (4, Some('1'))), (2, (5, None)))
+
+    val output = input1.leftOuterJoin(input2)
+
+    Assert.assertEquals(output.collect(), expectedOutput)
+  }
+
+  @Test
+  def testGroupWith() { // alias Co-group
+    val input1 = sc.makeRDD(Seq((1, 4), (2, 5)))
+    val input2 = sc.makeRDD(Seq((1, '1'), (2, '2')))
+    val input3 = sc.makeRDD(Seq((1, "one"), (2, "two")))
+    val expectedOutput = Array(
+      (1, (Iterable(4), Iterable('1'), Iterable("one"))),
+      (2, (Iterable(5), Iterable('2'), Iterable("two")))
+    )
+
+    val output = input1.groupWith[Char, String](input2, input3)
 
     Assert.assertEquals(output.collect(), expectedOutput)
   }
