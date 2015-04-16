7a7232860b198e0bc99b13b91d86e542684a9f13
oleewere <oleewere@gmail.com>
2015-04-16 16:02:08 +0200

    handle Double.NaN with d2d function

--- spark-correlation/src/main/scala/com/sequenceiq/spark/job/CorrelationJob.scala
+++ spark-correlation/src/main/scala/com/sequenceiq/spark/job/CorrelationJob.scala
@@ -6,6 +6,9 @@ abstract class CorrelationJob {
 
   def computeCorrelation(input: RDD[String]) : Array[(Int, Int, Double)]
 
-  def d2d(d: Double) : Double = new java.text.DecimalFormat("#.######").format(d).toDouble
+  def d2d(d: Double) : Double = d.isNaN match {
+    case false => new java.text.DecimalFormat("#.######").format(d).toDouble
+    case _ => d
+  }
 
 }
